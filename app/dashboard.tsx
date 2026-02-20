6c6384f16bd9565c513ecc585fd7ee4450f61756
reuben-philip <reubenaphilip@gmail.com>
2026-02-20 12:11:26 -0800

    made it so that the app can get the users name for a more personalized message

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -1,16 +1,27 @@
+"use client"
+
 import Link from "next/link";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
 
 export default function Dashboard() {
+
+  const { user, isLoaded } = useUser();
+
+  const name = user?.firstName;
+
   return (
     <div className="home-page">
       <header>
-        <h1 className="login-title">Welcome</h1>
-        <UserButton />
+        <h1 className="login-title">Welcome {name} </h1>
       </header>
 
+      <div className = "user-button">
+        <UserButton />
+      </div>
+
       <div className="input-container"> 
-        <input type = "text" placeholder="What would you like to complete today"></input>
+        <input className="search-bar" type = "text" placeholder="What would you like to complete today"></input>
       </div>
     </div>
   );
