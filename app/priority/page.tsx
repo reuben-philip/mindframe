61d435156840db25f3892de5089aa1c196628e29
reuben-philip <reubenaphilip@gmail.com>
2026-03-02 15:20:12 -0800

    updated the priority page to have an outline

--- app/priority/page.tsx
+++ app/priority/page.tsx
@@ -1,5 +1,9 @@
+"use client"
 import React from 'react';
 import Link from "next/link";
+import { UserButton, useUser } from "@clerk/nextjs";
+import "../globals.css";
+
 export default function Priority() {
     return (
         <div>
@@ -11,7 +15,27 @@ export default function Priority() {
                 <Link className = "nav-link" href="/calender">Calendar</Link>
                 </nav>
             </div>
-            <h1>Priority Component</h1>
+            <div className="user-button">
+                <UserButton />
+            </div>
+
+            <div className="priority-email-page-grid">
+
+                <div className="priority-email-card">
+                    <div className="priority-email-header"><h2>Priority Emails</h2></div>
+                    <div className="priotiy-email-card-body">
+                        <p>Email 1</p>
+                        <p>Email 2</p>
+                        <p>Email 3</p>
+                    </div>
+                </div>
+
+                <div className="priority-email-right-column">
+                    <div className="view-card"></div>
+                    <input className="search-bar" type = "text" placeholder="who would you like to email today"></input>
+                </div>
+            </div>
+
         </div>
     );
 }
